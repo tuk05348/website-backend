e224c123e6321c6ac4f820d07f7ab184c761f88c
Niaz Baharudeen <tuk05348@temple.edu>
2025-02-12 23:59:48 -0500

    Initial AWS SAM stack with basic Lambda function for Cloud Resume Challenge.

---
+++ tests/unit/test_handler.py
@@ -0,0 +1,47 @@
+from moto import mock_aws
+from boto3 import resource
+from ...visitor_count.VisitorCount import create, read, update
+
+key = {"Key" : {"visitor-count-id" : "1"}}
+item = {"Item": {"visitor-count-id": "1", "number": "0" }}
+updated_item = {"Key": {"visitor-count-id": "1"},
+                "UpdateExpression": "SET #num = :newNum",
+                "ExpressionAttributeNames": {
+                    "#num": "number"
+                },
+                "ExpressionAttributeValues": {
+                    ":newNum": "1"
+                }
+            }
+
+@mock_aws
+def createDB():
+    dynamodb = resource("dynamodb", region_name="us-east-1")
+    dynamodb.create_table(
+        TableName = "visitor-count",
+        KeySchema=[{"AttributeName": "visitor-count-id", "KeyType": "HASH"}],
+        AttributeDefinitions=[{"AttributeName": "visitor-count-id", "AttributeType": "S"}],
+        BillingMode='PAY_PER_REQUEST'
+    )
+    return dynamodb.Table("visitor-count")
+
+@mock_aws
+def testRead():
+    db = createDB()
+    """
+    Test if reading from database fails when item that does not exist is read
+    """
+    assert int(read(db, key)["ResponseMetadata"]["HTTPStatusCode"]) == 200
+
+@mock_aws
+def testCreate():
+    db = createDB()
+    create(db, item)
+    assert int(read(db, key)["Item"]["number"]) == 0
+
+@mock_aws
+def testUpdate():
+    db = createDB()
+    create(db, item)
+    update(db, updated_item)
+    assert int(read(db, key)["Item"]["number"]) == 1
---
+++ visitor_count/VisitorCount.py
@@ -0,0 +1,56 @@
+import boto3
+import os
+import json
+from decimal import Decimal
+
+class DecimalEncoder(json.JSONEncoder):
+  def default(self, obj):
+    if isinstance(obj, Decimal):
+      return str(obj)
+    return json.JSONEncoder.default(self, obj)
+
+# Define the DynamoDB table that Lambda will connect to
+table_name = os.environ['DB_NAME']
+    # Create the DynamoDB resource
+dynamo = boto3.resource('dynamodb').Table(table_name)
+
+# Define some functions to perform the CRUD operations
+def create(db, payload):
+    return db.put_item(Item=payload['Item'])
+
+def read(db, payload):
+    return db.get_item(Key=payload['Key'])
+
+def update(db, payload):
+    return db.update_item(**{k: payload[k] for k in ['Key', 'UpdateExpression', 
+    'ExpressionAttributeNames', 'ExpressionAttributeValues'] if k in payload})
+
+operations = {
+    'create': create,
+    'read': read,
+    'update': update
+}
+
+def lambda_handler(event, context):
+    '''Provide an event that contains the following keys:
+      - operation: one of the operations in the operations dict below
+      - payload: a JSON object containing parameters to pass to the 
+        operation being performed
+    '''
+    event = json.loads(event['body'])
+    operation = event['operation']
+    payload = event['payload']
+
+    if operation in operations:
+        return {
+                'statusCode': 200,
+                'headers': {
+                            "Access-Control-Allow-Headers" : "Content-Type",
+                            "Access-Control-Allow-Origin": "*",
+                            "Access-Control-Allow-Methods": "POST"
+                            },
+                'body': json.dumps(operations[operation](dynamo, payload), cls=DecimalEncoder),
+                'isBase64Encoded': False
+                }
+    else:
+        raise ValueError(f'Unrecognized operation "{operation}"')
