d691969f87a68ed0d4e7d8fc04f02d84c28bb286
Niaz Baharudeen <tuk05348@temple.edu>
2025-02-13 10:28:00 -0500

    Created first integration test to ensure the REST API works and responds.

--- tests/integration/test_api_gateway.py
+++ tests/integration/test_api_gateway.py
@@ -3,6 +3,7 @@ import os
 import boto3
 import pytest
 import requests
+import json
 
 """
 Make sure env variable AWS_SAM_STACK_NAME exists with the name of the stack we are going to test. 
@@ -30,16 +31,15 @@ class TestApiGateway:
 
         stacks = response["Stacks"]
         stack_outputs = stacks[0]["Outputs"]
-        api_outputs = [output for output in stack_outputs if output["OutputKey"] == "HelloWorldApi"]
+        api_outputs = [output for output in stack_outputs if output["OutputKey"] == "VisitorCountApi"]
 
         if not api_outputs:
-            raise KeyError(f"HelloWorldAPI not found in stack {stack_name}")
+            raise KeyError(f"VisitorCountAPI not found in stack {stack_name}")
 
         return api_outputs[0]["OutputValue"]  # Extract url from stack outputs
 
     def test_api_gateway(self, api_gateway_url):
         """ Call the API Gateway endpoint and check the response """
-        response = requests.get(api_gateway_url)
+        response = requests.post(api_gateway_url, json.dumps({"operation": "read", "payload": {"Key": {"visitor-count-id" : "1"}}}))
 
         assert response.status_code == 200
-        assert response.json() == {"message": "hello world"}
