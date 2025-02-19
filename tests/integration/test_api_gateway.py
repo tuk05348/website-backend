a20087ce55e6ab861a45c8ecb852df95cd2665b4
Niaz Baharudeen <tuk05348@temple.edu>
2025-02-18 23:48:51 -0500

    Fixed import in integration test and decided to remove deletion of staging stack.

--- tests/integration/test_api_gateway.py
+++ tests/integration/test_api_gateway.py
@@ -3,7 +3,7 @@ import json
 import requests
 import boto3
 import pytest
-from ...visitor_count.VisitorCount import DecimalEncoder
+from visitor_count.VisitorCount import DecimalEncoder
 from .. import key, item, updated_item
 """
 Make sure env variable AWS_SAM_STACK_NAME exists with the name of the stack we are going to test. 
