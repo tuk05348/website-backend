c1a46b2efeb2e3c0502ef73524d9c6ce05187c80
Niaz Baharudeen <tuk05348@temple.edu>
2025-02-22 22:24:52 -0500

    Edited unit test to use the mock database as a fixture. Used yield to prevent mock db from being torn down after the create function finishes.

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,36 +1,35 @@
+from pytest import fixture
 from moto import mock_aws
 from boto3 import resource
 from visitor_count.VisitorCount import create, read, update
 from .. import key, item, updated_item
 
-@mock_aws
+@fixture
 def createDB():
-    dynamodb = resource("dynamodb", region_name="us-east-1")
-    dynamodb.create_table(
-        TableName = "visitor-count",
-        KeySchema=[{"AttributeName": "visitor-count-id", "KeyType": "HASH"}],
-        AttributeDefinitions=[{"AttributeName": "visitor-count-id", "AttributeType": "S"}],
-        BillingMode='PAY_PER_REQUEST'
-    )
-    return dynamodb.Table("visitor-count")
+	with mock_aws():
+		dynamodb = resource("dynamodb", region_name="us-east-1")
+		dynamodb.create_table(
+        	TableName = "visitor-count",
+        	KeySchema=[{"AttributeName": "visitor-count-id", "KeyType": "HASH"}],
+        	AttributeDefinitions=[{"AttributeName": "visitor-count-id", "AttributeType": "S"}],
+        	BillingMode='PAY_PER_REQUEST'
+    	)
+		yield dynamodb.Table("visitor-count")
 
 @mock_aws
-def testRead():
-    db = createDB()
+def testRead(createDB):
     """
     Test if reading from database fails when item that does not exist is read
     """
-    assert int(read(db, key)["ResponseMetadata"]["HTTPStatusCode"]) == 200
+    assert int(read(createDB, key)["ResponseMetadata"]["HTTPStatusCode"]) == 200
 
 @mock_aws
-def testCreate():
-    db = createDB()
-    create(db, item)
-    assert int(read(db, key)["Item"]["number"]) == 0
+def testCreate(createDB):
+    create(createDB, item)
+    assert int(read(createDB, key)["Item"]["number"]) == 0
 
 @mock_aws
-def testUpdate():
-    db = createDB()
-    create(db, item)
-    update(db, updated_item)
-    assert int(read(db, key)["Item"]["number"]) == 1
+def testUpdate(createDB):
+    create(createDB, item)
+    update(createDB, updated_item)
+    assert int(read(createDB, key)["Item"]["number"]) == 1
