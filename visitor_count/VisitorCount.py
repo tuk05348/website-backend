bb8eb209e07447d8dbe405f41b50d5adaf1bb157
Niaz Baharudeen <tuk05348@temple.edu>
2025-02-26 15:47:50 -0500

    Updated lambda function so it can update the visitor count or create the count in one call rather than using get_item and put_item by using an update expression to increment the value.

--- visitor_count/VisitorCount.py
+++ visitor_count/VisitorCount.py
@@ -3,26 +3,21 @@ import os
 import json
 
 def getAndUpdateCount(db):
-	"""
-	Get the visitor count from the databse if it exists, if it does not, create an entry
-	in the database. If it does, update the value in the database. Return the created/updated value
-	"""
-	response = db.get_item(Key={"visitor-count-id" : "1"})
-	updated_count = 1
-	if "Item" not in response:
-		db.put_item(Item={"visitor-count-id": "1", "number": "1" })
-	else:
-		updated_count = int(response["Item"]["number"]) + 1
-		db.update_item(**{ "Key" : {"visitor-count-id": "1"},
-                    "UpdateExpression" : "SET #num = :newNum",
-                    "ExpressionAttributeNames" : {"#num": "number"},
-                    "ExpressionAttributeValues" : {
-                        ":newNum": updated_count
-                    }
-                }
-        )
-
-	return {'visitor_count': updated_count}
+    """
+    Get the visitor count from the databse if it exists, if it does not, create an entry
+    in the database. If it does, update the value in the database. Return the created/updated value
+    """
+    return db.update_item(
+            **{ "Key" : {"visitor-count-id": "1"},
+                "UpdateExpression" : "SET #num = if_not_exists(#num, :zero) + :inc",
+                "ExpressionAttributeNames" : {"#num": "visitor-count"},
+                "ExpressionAttributeValues" : {
+                    ":inc": 1,
+                    ":zero": 0
+                },
+                "ReturnValues" : "UPDATED_NEW"
+            }
+        )["Attributes"]
 
 def lambda_handler(event, context):
     '''Provide an event that contains the following keys:
